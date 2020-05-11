5f85f751b37aba4c3fcc0d68659067395cb624d0
manson.zhou <manson.zhou@sunvalley.com.cn>
2020-05-11 16:20:42 +0800

    feat: 布局demo提交。

--- src/router/index.js
+++ src/router/index.js
@@ -28,6 +28,46 @@ const routes = [
     path: '/fade/collapse展开折叠',
     name: 'collapse展开折叠',
     component: () => import('../views/fade/collapse展开折叠.vue')
+  },
+  {
+    path: '/layout/基础布局',
+    name: 'layout基础布局',
+    component: () => import('../views/layout/基础布局.vue')
+  },
+  {
+    path: '/layout/分栏间隔',
+    name: 'layout分栏间隔',
+    component: () => import('../views/layout/分栏间隔.vue')
+  },
+  {
+    path: '/layout/混合布局',
+    name: 'layout混合布局',
+    component: () => import('../views/layout/混合布局.vue')
+  },
+  {
+    path: '/layout/分栏偏移',
+    name: 'layout分栏偏移',
+    component: () => import('../views/layout/分栏偏移.vue')
+  },
+  {
+    path: '/layout/对齐方式',
+    name: 'layout对齐方式',
+    component: () => import('../views/layout/对齐方式.vue')
+  },
+  {
+    path: '/layout/响应式布局',
+    name: 'layout响应式布局',
+    component: () => import('../views/layout/响应式布局.vue')
+  },
+  {
+    path: '/layout/Container布局容器',
+    name: 'layoutContainer布局容器',
+    component: () => import('../views/layout/Container布局容器.vue')
+  },
+  {
+    path: '/layout/Container导航',
+    name: 'layoutContainer导航',
+    component: () => import('../views/layout/Container导航.vue')
   }
 ]
 
